961e6f838aafbfed05af72980c3f5e591623442a
Jacob <reyno120@purdue.edu>
2020-07-08 13:02:09 -0400

    adding config

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://jmreynolds:jmreynolds@mycluster-pjizw.mongodb.net/CommunityRecipes?retryWrites=true&w=majority',
+    sessionSecret: 'plants',
+    jwtKey: 'plants'
+};
