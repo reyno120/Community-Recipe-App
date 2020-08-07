ee0a6601995689d54d7a1d04b175ec6ed9663ae3
Jacob <reyno120@purdue.edu>
2020-08-07 11:47:27 -0400

    discover ux work

--- client/src/components/Discover.jsx
+++ client/src/components/Discover.jsx
@@ -13,6 +13,9 @@ import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 import SearchIcon from '@material-ui/icons/Search';
 import InputAdornment from "@material-ui/core/InputAdornment";
 import IconButton from '@material-ui/core/IconButton';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import Collapse from '@material-ui/core/Collapse';
+import Slider from '@material-ui/core/Slider';
 
 
 const theme = createMuiTheme({
@@ -37,7 +40,8 @@ class Discover extends Component {
         difficulty: '',
         time: 0,
         recipes: [],
-        search: ''
+        search: '',
+        expanded: false
     };
 
 
@@ -51,6 +55,8 @@ class Discover extends Component {
             var index = allergens.indexOf(e.target.name);
             allergens.splice(index, 1);
         }
+
+        this.handleSubmit()
     }
 
     onChange = (e) => {
@@ -65,6 +71,8 @@ class Discover extends Component {
             }
         }
         this.setState({ingredients: ingredients});
+
+        this.handleSubmit();
     }
 
     handleSubmit = () => {
@@ -93,7 +101,24 @@ class Discover extends Component {
         });
     }
 
+    valuetext = (value) => {
+        return`${value}`;
+    }
+
     render() { 
+
+        const styles = {
+            expand: {
+                transform: 'rotate(0deg)'
+            },
+            expandOpen: {
+                transform: 'rotate(180deg)'
+            },
+            checkbox: {
+                marginTop: '-2em'
+            }
+        }
+
         return (  
             <div>
                 <Paper
@@ -103,7 +128,7 @@ class Discover extends Component {
                         width: '1366px',
                         margin: 'auto'
                     }}>
-                    <h2 style={{textAlign: 'center'}}>Discover New Recipes!</h2>
+                    <h2 style={{textAlign: 'center', paddingTop: '.5em', marginBottom: '2em'}}>Discover New Recipes!</h2>
                     <Grid container>
 
                         <Grid align="center" item xs={12}>
@@ -121,7 +146,7 @@ class Discover extends Component {
                                     onKeyDown={this.checkKey}
                                     onChange={this.onChange}
                                     name="search"
-                                    style={{width: '30em'}}
+                                    style={{width: '38em'}}
                                     color="primary"
                                 ></TextField>
                             </ThemeProvider>
@@ -133,35 +158,53 @@ class Discover extends Component {
                                                                 marginLeft: '1em',
                                                                 marginTop: '1em',
                                                                 paddingLeft: '1em',
-                                                                border: 'solid 2px rgb(254, 98, 57)',
-                                                                borderRadius: '10px'
-                                                                }}>
-                            <h2>Allergens:</h2>
-                            <div style={{textAlign: 'left', marginLeft: '3em'}}>
-                                <p><Checkbox name="saltFree" onChange={this.handleChange} checked={this.state.saltFree}/>no added salt</p>
-                                <p><Checkbox name="oilFree" onChange={this.handleChange} checked={this.state.oilFree}/>oil-free</p>
-                                <p><Checkbox name="sugarFree" onChange={this.handleChange} checked={this.state.sugarFree}/>no added sugar</p>
-                                <p><Checkbox name="glutenFree" onChange={this.handleChange} />gluten free</p>
-                                <p><Checkbox name="nutFree" onChange={this.handleChange} />nut free</p>
-                                <p><Checkbox name="peanutFree" onChange={this.handleChange} />peanut free</p>
-                                <p><Checkbox name="soyFree" onChange={this.handleChange} />soy free</p>
-                            </div>
+                                                                }}>     
+                            <div style={{ border: 'solid 2px rgb(254, 98, 57)', borderRadius: '10px'}}> 
+                            <div style={{height: '.5em'}}></div>                                
+                            <h2 style={{display: 'inline', marginLeft: '.5em'}}>Allergens:</h2>
+                            <IconButton style={{marginLeft: '1em'}} size="small" onClick={() => this.setState({expanded: !this.state.expanded})}>
+                                <ExpandMoreIcon style={this.state.expanded ? styles.expandOpen : styles.expand} />
+                            </IconButton> 
+                            <Collapse in={this.state.expanded} timeout="auto" unmountOnExit>
+                                <div style={{textAlign: 'left', marginLeft: '3em'}}>
+                                    <p><Checkbox name="saltFree" onChange={this.handleChange} checked={this.state.saltFree}/>no added salt</p>
+                                    <p style={styles.checkbox}><Checkbox name="oilFree" onChange={this.handleChange} checked={this.state.oilFree}/>oil-free</p>
+                                    <p style={styles.checkbox}><Checkbox name="sugarFree" onChange={this.handleChange} checked={this.state.sugarFree}/>no added sugar</p>
+                                    <p style={styles.checkbox}><Checkbox name="glutenFree" onChange={this.handleChange} />gluten free</p>
+                                    <p style={styles.checkbox}><Checkbox name="nutFree" onChange={this.handleChange} />nut free</p>
+                                    <p style={styles.checkbox}><Checkbox name="peanutFree" onChange={this.handleChange} />peanut free</p>
+                                    <p style={styles.checkbox}><Checkbox name="soyFree" onChange={this.handleChange} />soy free</p>
+                                </div>
+                            </Collapse>
 
 
                         {/*********** Time ***********/}
-                            <h2>Time</h2>
+                            <h2 style={{marginLeft: '.5em'}}>Time:</h2>
                             <ThemeProvider theme={theme}>
-                                <p style={{display: 'inline'}}>Less than</p>
+                                {/* <p style={{display: 'inline'}}>Less than</p>
                                 <TextField name="time" inputProps={{min: 0}} style={{width: '4em'}} onChange={this.onChange} type="number"></TextField>
-                                <p style={{display: 'inline'}}>minutes</p>
+                                <p style={{display: 'inline'}}>minutes</p> */}
+                                <div style={{textAlign: 'center'}}>
+                                    <Slider
+                                        style={{width: '18em', marginTop: '-1.5em'}}
+                                        defaultValue={0}
+                                        step={5}
+                                        marks
+                                        min={0}
+                                        max={120}
+                                        getAriaValueText={this.valuetext}
+                                        valueLabelDisplay="auto"
+                                        color="primary"
+                                        onChange={this.onChange}
+                                    />
+                                </div>
                             </ThemeProvider>
 
 
                         {/*********** Difficulty ***********/}
-                            <h2>Difficulty</h2>
-
+                            <h2 style={{marginLeft: '.5em'}}>Difficulty:</h2>
                             <div style={{display: 'block'}}>
-                                <p style={{display: 'inline'}}>Easy</p>
+                                <p style={{display: 'inline', marginLeft: '1.9em'}}>Easy</p>
                                 <Radio
                                     checked={this.state.difficulty === 'Easy'}
                                     value={"Easy"}
@@ -171,7 +214,7 @@ class Discover extends Component {
                             </div>
 
                             <div style={{display: 'block'}}>
-                                <p style={{display: 'inline'}}>Medium</p>
+                                <p style={{display: 'inline', marginLeft: '.5em'}}>Medium</p>
                                 <Radio
                                     checked={this.state.difficulty === 'Medium'}
                                     value={"Medium"}
@@ -181,7 +224,7 @@ class Discover extends Component {
                             </div>
 
                             <div style={{display: 'block'}}>
-                                <p style={{display: 'inline'}}>Hard</p>
+                                <p style={{display: 'inline', marginLeft: '1.9em'}}>Hard</p>
                                 <Radio
                                     checked={this.state.difficulty === 'Hard'}
                                     value={"Hard"}
@@ -191,14 +234,21 @@ class Discover extends Component {
                             </div>
 
                             {/*********** Ingredients ***********/}
-                            <h2>Ingredients:</h2>
-                            <Select 
-                                isMulti
-                                onChange={this.handleIngredients}
-                                options={IngredientList}
-                                isSearchable={true}
-                            />
-                        </Grid>
+                            <h2 style={{marginLeft: '.5em'}}>Ingredients:</h2>
+                            <div style={{width: '18em', margin: 'auto'}}>
+                                <Select 
+                                    isMulti
+                                    onChange={this.handleIngredients}
+                                    options={IngredientList}
+                                    isSearchable={true}
+                                />
+                                </div>
+                            <div style={{height: '.5em'}}></div>
+                        </div>
+                        </Grid> 
+
+
+                        {/*********** Results ***********/}
                         <Grid item xs={8}>
                         <h2 style={{marginLeft: '1em'}}>Displaying {this.state.recipes.length} results</h2>
                         <Grid spacing={3} style={{margin: 'auto'}} container>
@@ -223,9 +273,9 @@ class Discover extends Component {
                     </Grid>
                         </Grid>
                     </Grid>
-                    <Button variant="outlined" style={{margin: '2em auto 2em auto'}} onClick={this.handleSubmit}>Submit</Button> 
+                    {/* <Button variant="outlined" style={{margin: '2em auto 2em auto'}} onClick={this.handleSubmit}>Submit</Button>  */}
+                    <div style={{height: '2em'}}></div>
                 </Paper>
-                <div style={{height: '2em'}}></div>
             </div>
         );
     }
