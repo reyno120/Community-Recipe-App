05e103db21f3b07ea6d3c64fbd3447872c2b7a9c
Jacob <reyno120@purdue.edu>
2020-08-09 09:20:53 -0400

    adding config to heroku

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://jmreynolds:jmreynolds@mycluster-pjizw.mongodb.net/CommunityRecipes?retryWrites=true&w=majority',
+    jwtKey: 'plants',
+    emailPassword: 'plantLover6751',
+    myEmail: 'jmreynolds03@gmail.com'
+}
